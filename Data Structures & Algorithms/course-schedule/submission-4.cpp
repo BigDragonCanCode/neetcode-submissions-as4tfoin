6308e4bef5842b4159b4e0b39fa52d4bbca0f72e
BigDragonCanCode <100175669+BigDragonCanCode@users.noreply.github.com>
2026-04-24 00:51:33 -0700

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    unordered_set<int> visited;
+    vector<vector<int>> l;
+
+public:
+    bool helper(int currNum) {
+        if (visited.count(currNum) != 0) //cycle found
+            return false;
+        if (l[currNum].size() == 0) //there's no prerequisite
+            return true;
+        
+        visited.insert(currNum);
+        //for a course we need to satisfy all the prerequisites
+        for (int& i: l[currNum]) {
+            if (!helper(i))
+                return false;
+        }
+        visited.erase(currNum);
+        l[currNum].clear(); // we already know it's valid so we don't want to traverse through known valid path again
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //initialize adjacency list
+        for (int i=0; i<numCourses; i++) {
+            l.push_back({});
+        }
+        //build an adjancency list
+        for (vector<int>& p: prerequisites) {
+            l[p[0]].push_back(p[1]);
+        }
+
+
+        for (int i=0; i<numCourses; i++) {
+            if(!helper(i))
+                return false;
+            visited.clear();
+        }
+        return true;
+    }
+};
