3a24840ae307fa64d2131722c8d4ff5cb5bcbfe5
BigDragonCanCode <100175669+BigDragonCanCode@users.noreply.github.com>
2026-05-12 22:45:19 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> mem(nums.size());
+
+        //mem[i] meaning: the max you can get by robbing house 0-i
+        mem[0] = nums[0];
+        mem[1] = max(nums[0], nums[1]);
+
+        for (int i=2;i<nums.size();i++) {
+            //we can choose not rob house i: take the result mem[i-1]
+            //we can choose rob house i: take house i's money + the maximum money you can rob from the prev houses
+            mem[i] = max(mem[i-1], mem[i-2] + nums[i]);
+        }
+        return mem[nums.size()-1];
+    }
+};
