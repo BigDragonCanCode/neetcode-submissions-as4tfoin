ec86a4212156316ab8041795016267877babf376
BigDragonCanCode <100175669+BigDragonCanCode@users.noreply.github.com>
2026-05-24 19:53:20 -0700

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        map<int, int> mp;
+        for (auto& i: intervals) {
+            mp[i.start]++;
+            mp[i.end]--;
+        }
+        int count = 0;
+        int ans = 0;
+
+        for(auto& [key,val]: mp) {
+            count += val;
+            ans = max(ans, count);
+        }
+        return ans;
+    }
+};
