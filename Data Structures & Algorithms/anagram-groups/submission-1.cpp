11b15e6eb556d0861d3c8443c8cce525b8979e64
BigDragonCanCode <100175669+BigDragonCanCode@users.noreply.github.com>
2026-05-25 11:53:04 -0700

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,27 @@
+
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int> dict; //word, index
+        vector<vector<string>> results;
+        int size = 0;
+
+        //O(n* klogk)
+        for (string str: strs) {
+            string s = str;
+            sort(s.begin(), s.end()); //O(k* logk)
+            
+            if (dict.find(s) != dict.end()) {
+                int idx = dict[s];
+                results[idx].push_back(str);
+            } else {
+                dict.emplace(s, size);
+                results.push_back({str});
+                size++;
+            }
+        }
+
+        return results;
+    }
+};
